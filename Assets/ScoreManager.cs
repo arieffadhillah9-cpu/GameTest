6badaa3df3b7ff4eff2c116053989cc5d44e9852
arieffadhillah9-cpu <arieffadhillah9@gmail.com>
2026-04-05 22:11:14 +0700

    update skor

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -6,6 +6,7 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI leftScoreText;
     [SerializeField] private TextMeshProUGUI rightScoreText;
 
+    [SerializeField] private int maxScore = 11;
     private int leftScore = 0;
     private int rightScore = 0;
 
